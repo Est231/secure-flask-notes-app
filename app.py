a6b98cd2b5213da0c56d14382199cece72e930c8
Est231 <fire.play.04@mail.ru>
2025-12-02 18:01:17 +0400

    test: add debug mode for security scan test

--- app.py
+++ app.py
@@ -656,4 +656,4 @@ if __name__ == '__main__':
     app_logger.info("=" * 60)
 
     # Запуск в продакшн режиме (debug=False)
-    app.run(debug=False, host='127.0.0.1', port=5001)
+    app.run(debug=True, host='127.0.0.1', port=5001)
